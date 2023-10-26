7d24ea47183ecf3b879314cfbc3cf60f6d087f05
noahnow2004 <34319642+noahnow2004@users.noreply.github.com>
2023-10-25 22:53:42 -0400

    Delete decode.py

--- decode.py
+++
@@ -1,7 +0,0 @@
-def decode(encoded_password):
-    templist = list(encoded_password)
-    newlist = []
-    for i in templist:
-        encoded_char = str(int(i) -3)
-        newlist.append(encoded_char)
-    return ''.join(newlist)
